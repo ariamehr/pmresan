830acfb69143703fa77de07c7cb5649feee60717
ariamehr <ariamehr.moradi@gmail.com>
2016-10-14 14:30:56 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '246929699:AAGlhwrpvZ_sEW5VlnC_wMift7PaGy3gpDU');
+define('BOT_TOKEN', '246929699:AAHFmy81vxyj9F7Hxhjg6VIBoSpBswAAoIs');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
